b49d5272de265cbf101bd02ac4d05b9313a4bfcc
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2022-06-20 19:20:06 -0300

    Impl random

--- ProyectoMicro/main.asm
+++ ProyectoMicro/main.asm
@@ -1,12 +1,47 @@
-;
-; ProyectoMicro.asm
-;
-; Created: 20/6/2022 18:35:36
-; Author : juan-
-;
+.ORG 0x0000
+    jmp start
 
+.DSEG
+    random_next: .BYTE 1
 
-; Replace with your application code
+.CSEG
+
 start:
-    inc r16
-    rjmp start
+    ; Inicializar sistema
+
+    ldi     XL,     LOW(random_next)
+    ldi     XH,     HIGH(random_next)
+    ldi     r16,    1
+
+loop:
+    call    random
+    rjmp    loop
+
+random:
+    push    r17
+    push    XH
+    push    XL
+
+    ldi     XL,     LOW(random_next)
+    ldi     XH,     HIGH(random_next)
+    ld      r16,    X
+
+    rol     r16
+    rol     r16
+
+    ldi     r17,    199
+    eor     r16,    r17
+
+    rol     r16
+    ldi     r17,    101
+    eor     r16,    r17
+
+    ldi     r17, 5
+    add     r16, r17
+
+    st      X, r16
+
+    pop     XL
+    pop     XH
+    pop     r17
+    ret
